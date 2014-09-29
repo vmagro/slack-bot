24ed15c60e146c50768e7eeac55e277cc53905a6
Vinnie Magro <vmagro@socaldevs.com>
2014-09-29 09:46:33 -0700

    Fix bot

--- bots/bro-sydney.js
+++ bots/bro-sydney.js
@@ -1,5 +1,5 @@
 module.exports = function(message, slackMetadata) {
-  if(message.indexOf('born mobile') != -1) {
+  if(message.indexOf('bro sydney') != -1) {
     //append the slack timestamp so it shows the preview every time
     return {
       text: 'https://scontent-a-lax.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/10157152_10202200513747625_7381961324784988088_n.jpg?oh=b468745f0fc62743751bfb46c9fe25ba&oe=5484AE9C&slack='+slackMetadata.timestamp
