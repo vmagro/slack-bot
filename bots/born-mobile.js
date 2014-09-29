1a3588da772c0a7be39fe698dffaf10ecfa30a94
Vinnie Magro <vmagro@socaldevs.com>
2014-09-29 09:39:44 -0700

    Show image preview every time by appending query string

--- bots/born-mobile.js
+++ bots/born-mobile.js
@@ -1,7 +1,8 @@
 module.exports = function(message, slackMetadata) {
   if(message.indexOf('born mobile') != -1) {
+    //append the slack timestamp so it shows the preview every time
     return {
-      text: 'http://www.extremetech.com/wp-content/uploads/2013/01/hipster-diva.jpg'
+      text: 'http://www.extremetech.com/wp-content/uploads/2013/01/hipster-diva.jpg?'+slackMetadata.timestamp
     };
   }
 };
