8b3f6e30e7e009f8c36913ba279829154ab4fb0a
Vinnie Magro <vmagro@socaldevs.com>
2014-09-29 15:50:48 -0700

    Remove ian trigger from dogeify

--- bots/dogeify.js
+++ bots/dogeify.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 module.exports = function(message, slackMetadata, callback) {
-  if(slackMetadata.user_name === 'ian' || (message.indexOf('dogeify') == 0)) {
+  if(message.indexOf('dogeify') == 0) {
     var toDogeify = message;
     if(toDogeify.indexOf('dogeify') == 0)
       toDogeify = toDogeify.substring(8);
