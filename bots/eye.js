3989ca5eea62a4dd1bc836f059c92cb57d19f348
Paul Moulton <pmoulton2@gmail.com>
2014-09-29 22:17:24 -0700

    Adding tan45

--- bots/eye.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(message, slackMetadata) {
-  if(message.indexOf('i') == 0) {
-    var newMessage = 'eye' + message.substring(1);
-    return {
-      text: newMessage
-    };
-  }
-};
