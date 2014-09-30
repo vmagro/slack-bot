a612ccaec53edfd01832ccc677ea201c31d57fc0
Vinnie Magro <vmagro@socaldevs.com>
2014-09-29 18:12:52 -0700

    Revert "yo bot"
    This reverts commit 3c513f3a847637d86f8e8d953d6d76c9d77ebb26.

--- bots/yo.js
+++
@@ -1,9 +0,0 @@
-var exec = require('child_process').exec;
-
-module.exports = function(message, slackMetadata, callback) {
-  if(message.indexOf('yo') === 0) {
-    return {
-      text: 'yo @' + slackMetadata.user_name
-    };
-  }
-};
