713b90d5d35237fd2178fc92cdf430fe1be11b39
Peter Zhang <p@peetahzee.com>
2014-09-29 18:12:36 -0700

    yo bot fixed

--- bots/yo.js
+++ bots/yo.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 module.exports = function(message, slackMetadata, callback) {
-  if(message.indexOf('yo') === 0) {
+  if(message.indexOf('yo') === 0 && slackMetadata.user_name != 'slackbot') {
     return {
       text: 'yo @' + slackMetadata.user_name
     };
