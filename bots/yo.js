73d29587b56a62a532709bab20a872d04d9c2088
Vinnie Magro <vmagro@socaldevs.com>
2014-09-30 19:20:19 -0700

    PTZ's yo bot

---
+++ bots/yo.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').exec;
+
+module.exports = function(message, slackMetadata, callback) {
+  if (message.indexOf('yo') === 0 && slackMetadata.user_name != 'slackbot') {
+    return {
+      text: 'yo @' + slackMetadata.user_name
+    };
+  }
+};
