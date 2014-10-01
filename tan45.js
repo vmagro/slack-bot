d817dd4287021782102fd088d9bff1e352aedb88
Vinnie Magro <vmagro@socaldevs.com>
2014-10-01 11:54:01 -0700

    Delete tan45.js in root

--- tan45.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(message, slackMetadata) {
-  if(message.indexOf('!') == -1) {
-    var newMessage = message + "!!!1tan45";
-    return {
-      text: newMessage
-    };
-  }
-};
