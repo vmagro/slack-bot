4e279a5876be95456ecf8fb393a3aaba4e2d7ebb
Vinnie Magro <vmagro@socaldevs.com>
2014-10-12 14:16:03 -0700

    Use shorter url

--- bots/bro-sydney.js
+++ bots/bro-sydney.js
@@ -2,7 +2,7 @@ module.exports = function(message, slackMetadata, callback) {
   if(message.indexOf('bro sydney') != -1) {
     //append the slack timestamp so it shows the preview every time
     callback({
-      text: 'https://scontent-a-lax.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/10157152_10202200513747625_7381961324784988088_n.jpg?oh=b468745f0fc62743751bfb46c9fe25ba&oe=5484AE9C&slack='+slackMetadata.timestamp
+      text: 'http://goo.gl/fiyPmH&slack='+slackMetadata.timestamp
     });
   }
 };
