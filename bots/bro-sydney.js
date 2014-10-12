0bcf01ca76196d30d30917911d5b939cf7233520
Vinnie Magro <vmagro@socaldevs.com>
2014-10-12 14:18:13 -0700

    Fix short url

--- bots/bro-sydney.js
+++ bots/bro-sydney.js
@@ -2,7 +2,7 @@ module.exports = function(message, slackMetadata, callback) {
   if(message.indexOf('bro sydney') != -1) {
     //append the slack timestamp so it shows the preview every time
     callback({
-      text: 'http://goo.gl/fiyPmH&slack='+slackMetadata.timestamp
+      text: 'http://goo.gl/fiyPmH?'+slackMetadata.timestamp
     });
   }
 };
