bae8e121b1e37b4fd39a9d4ebec2903fc8c6947b
Vinnie Magro <vmagro@socaldevs.com>
2015-01-12 20:10:21 -0800

    Change trigger for darth james

--- bots/james.js
+++ bots/james.js
@@ -1,5 +1,5 @@
 module.exports = function (message, slackMetadata, callback) {
-    if (message.indexOf('james') != -1) {
+    if (message.indexOf('darth james') != -1) {
         //append the slack timestamp so it shows the preview every time
         callback({
             text: 'http://bot.vinnie.io/james?' + slackMetadata.timestamp
