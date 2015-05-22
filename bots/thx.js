f40a8a686c3a4b3d6fd0b10e2f6abd34972774d3
Paul Moulton <pmoulton2@gmail.com>
2015-05-21 22:30:32 -0700

    Thx bot thanks you

---
+++ bots/thx.js
@@ -0,0 +1,10 @@
+module.exports = function (message, slackMetadata, callback) {
+    if (message.indexOf('thx') != -1) {
+        //append the slack timestamp so it shows the preview every time
+        callback({
+            rewrite: true,
+            text: 'http://i.ytimg.com/vi/FWkJ86JqlPA/hqdefault.jpg' + slackMetadata.timestamp
+        });
+    }
+    callback();
+};
