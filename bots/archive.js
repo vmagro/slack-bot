861e3b57d9a81c5b2451ca663b7d2f2a0aaacb28
Vinnie Magro <vmagro@socaldevs.com>
2015-12-01 13:23:54 -0800

    Make archive and points not crash in dev

--- bots/archive.js
+++ bots/archive.js
@@ -1,7 +1,14 @@
-var db = require('orchestrate')(process.env.ORCHESTRATE_TOKEN);
+if (process.env.ORCHESTRATE_TOKEN) {
+	var db = require('orchestrate')(process.env.ORCHESTRATE_TOKEN);
 
-module.exports = function (message, slackMetadata, callback) {
-    console.log(slackMetadata);
-    db.put('messages', slackMetadata.timestamp.toString(), slackMetadata);
-    callback();
-};
+	module.exports = function(message, slackMetadata, callback) {
+		console.log(slackMetadata);
+		db.put('messages', slackMetadata.timestamp.toString(), slackMetadata);
+		callback();
+	};
+} else {
+	module.exports = function(message, slackMetadata, callback) {
+		console.log('not archiving in dev');
+		callback();
+	}
+}
--- bots/points-bot.js
+++ bots/points-bot.js
@@ -1,74 +1,76 @@
-/**
- * Created by vmagro on 7/16/15.
- */
+if (process.env.ORCHESTRATE_TOKEN) {
+	var db = require('orchestrate')(process.env.ORCHESTRATE_TOKEN);
 
-var db = require('orchestrate')(process.env.ORCHESTRATE_TOKEN);
+	module.exports = function (message, slackMetadata, callback) {
+		if (message.indexOf('++') !== -1) {
+			var thing = findThing(message, '++');
+			if (thing == slackMetadata.user_name) {
+				callback({
+					text: "voting for yourself, really?"
+				});
+			} else {
+				applyPoint(thing, 1, function (val) {
+					var text = thing + ' has ' + val + ' point';
+					if (val !== 1) {
+						text += 's';
+					}
+					callback({
+						text: text
+					});
+				});
+			}
+		}
+		if (message.indexOf('--') !== -1) {
+			var thing = findThing(message, '--');
+			applyPoint(thing, -1, function (val) {
+				callback({
+					text: thing + ' has ' + val + ' points'
+				});
+			});
+		}
 
-module.exports = function (message, slackMetadata, callback) {
-  if (message.indexOf('++') !== -1) {
-    var thing = findThing(message, '++');
-    if (thing == slackMetadata.user_name) {
-      callback({
-        text: "voting for yourself, really?"
-      });
-    } else {
-      applyPoint(thing, 1, function (val) {
-        var text = thing + ' has ' + val + ' point';
-        if (val !== 1) {
-          text += 's';
-        }
-        callback({
-          text: text
-        });
-      });
-    }
-  }
-  if (message.indexOf('--') !== -1) {
-    var thing = findThing(message, '--');
-    applyPoint(thing, -1, function (val) {
-      callback({
-        text: thing + ' has ' + val + ' points'
-      });
-    });
-  }
+		if (message.indexOf('++') === -1 && message.indexOf('--') === -1) {
+			callback();
+		}
 
-  if (message.indexOf('++') === -1 && message.indexOf('--') === -1) {
-    callback();
-  }
+	};
 
-};
+	function applyPoint(thing, val, cb) {
+		//this code is really gross but it gets the job done
+		db.get('points', thing)
+			.then(function (res) {
+				var pointsObj = res.body;
+				if (pointsObj) {
+					console.log(pointsObj);
+					db.put('points', thing, {
+						point: pointsObj.point + val
+					});
+					cb(pointsObj.point + val);
+				} else {
+					db.put('points', thing, {
+						point: val
+					});
+					cb(val);
+				}
+			})
+			.fail(function (err) {
+				db.put('points', thing, {
+					point: val
+				});
+				cb(val);
+			});
+	}
 
-function applyPoint(thing, val, cb) {
-  //this code is really gross but it gets the job done
-  db.get('points', thing)
-    .then(function (res) {
-      var pointsObj = res.body;
-      if (pointsObj) {
-        console.log(pointsObj);
-        db.put('points', thing, {
-          point: pointsObj.point + val
-        });
-        cb(pointsObj.point + val);
-      } else {
-        db.put('points', thing, {
-          point: val
-        });
-        cb(val);
-      }
-    })
-    .fail(function (err) {
-      db.put('points', thing, {
-        point: val
-      });
-      cb(val);
-    });
-}
-
-function findThing(message, token) {
-  var tokenIndex = message.indexOf(token);
-  var startIndex = message.lastIndexOf(' ', tokenIndex);
-  if (startIndex !== -1) {
-    startIndex++;
-  }
-  return message.substring(startIndex, tokenIndex);
+	function findThing(message, token) {
+		var tokenIndex = message.indexOf(token);
+		var startIndex = message.lastIndexOf(' ', tokenIndex);
+		if (startIndex !== -1) {
+			startIndex++;
+		}
+		return message.substring(startIndex, tokenIndex);
+	}
+} else {
+	module.exports = function (message, slackMetadata, callback) {
+		console.log('skipping points bot in dev');
+	}
 }
